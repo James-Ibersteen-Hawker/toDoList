870fa78d918895863726aaf89568fc28333e5931
James-Ibersteen-Hawker <526rserbinenko@frhsd.com>
2025-03-04 11:50:27 -0500

    finished

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 "use strict";
+import { bezier } from "./beziers.js";
 let tasks = [];
 let counter = 0;
 get("#addTaskBtn").addEventListener("click", task);
 function task() {
-  let inp = get("#taskInput");
-  let value = inp.value;
-  inp.value = "";
-  if (!value) return;
+  if (!get("#taskInput").value) return;
+  let value = get("#taskInput").value;
+  get("#taskInput").value = "";
   counter++;
   get("#counter").textContent = `Total Tasks: ${counter}`;
   tasks.push(value);
-  let ul = get("#taskList");
-  let li = `<li class="list-group-item d-flex justify-content-between align-items-center" id="li_${
-    tasks.length - 1
-  }"><span id="sp_${tasks.length - 1}">${
-    tasks[tasks.length - 1]
-  }</span><span><button class="btn btn-success btn-sm" id="li_${
-    tasks.length - 1
-  }_done"> √ </button><button class="btn btn-danger btn-sm d-none" id="li_${
-    tasks.length - 1
-  }_undone"> X </button>  <button class="btn btn-warning btn-sm" id="li_${
-    tasks.length - 1
-  }_del">❌</button></span></li>`;
-  ul.insertAdjacentHTML("beforeend", li);
-  li = get(`#li_${tasks.length - 1}_del`);
-  li.addEventListener("click", () => {
-    let index = li.id.split("_")[1];
-    li = get(`#li_${index}`);
-    let listText = li.textContent.split(" ")[0].trim();
-    li.remove();
+  get("#taskList").insertAdjacentHTML(
+    "beforeend",
+    `<li class="list-group-item d-flex justify-content-between align-items-center" id="li_${
+      tasks.length - 1
+    }"><span id="sp_${tasks.length - 1}">${
+      tasks[tasks.length - 1]
+    }</span><span><button class="btn btn-success btn-sm" id="li_${
+      tasks.length - 1
+    }_done"> √ </button><button class="btn btn-danger btn-sm d-none" id="li_${
+      tasks.length - 1
+    }_undone"> X </button>  <button class="btn btn-warning btn-sm" id="li_${
+      tasks.length - 1
+    }_del">❌</button></span></li>`
+  );
+  get(`#li_${tasks.length - 1}_del`).addEventListener("click", (event) => {
+    tasks.splice(
+      tasks.indexOf(
+        event.target.parentElement.parentElement.getElementsByTagName("SPAN")[0]
+          .textContent
+      ),
+      1
+    );
     counter--;
     get("#counter").textContent = `Total Tasks: ${counter}`;
-    index = tasks.indexOf(listText);
-    tasks.splice(index, 1);
+    event.target.parentElement.parentElement.remove();
   });
-  li = get(`#li_${tasks.length - 1}_done`);
-  li.addEventListener("click", (event) => {
-    let div =
-      event.target.parentElement.parentElement.getElementsByTagName("SPAN")[0];
-    div.classList.toggle("strike");
+  get(`#li_${tasks.length - 1}_done`).addEventListener("click", (event) => {
+    event.target.parentElement.parentElement
+      .getElementsByTagName("SPAN")[0]
+      .classList.toggle("strike");
     event.target.classList.toggle("d-none");
     event.target.parentElement
       .getElementsByTagName("BUTTON")[1]
       .classList.toggle("d-none");
   });
-  li = get(`#li_${tasks.length - 1}_undone`);
-  li.addEventListener("click", (event) => {
-    let div =
-      event.target.parentElement.parentElement.getElementsByTagName("SPAN")[0];
-    div.classList.toggle("strike");
+  get(`#li_${tasks.length - 1}_undone`).addEventListener("click", (event) => {
+    event.target.parentElement.parentElement
+      .getElementsByTagName("SPAN")[0]
+      .classList.toggle("strike");
     event.target.classList.toggle("d-none");
     event.target.parentElement
       .getElementsByTagName("BUTTON")[0]
@@ -58,8 +58,7 @@ function task() {
 get("#clearBtn").addEventListener("click", () => {
   counter = 0;
   get("#counter").textContent = `Total Tasks: ${counter}`;
-  let ul = get("#taskList");
-  ul.innerHTML = "";
+  get("#taskList").innerHTML = "";
   tasks = [];
 });
 function get(param) {
